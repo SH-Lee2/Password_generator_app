a3defee22da367b94e4a56db0b0fe56bb6e3fc6c
이시형 <tlgud585@gmail.com>
2022-10-26 15:22:27 +0900

    fix: tailwind 적용 안됨 및 config 정의 vite 전용으로 tailwind.config 수정, postcss 확장자 변경,  colors , screens custom

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      'black' : '#18171F',
+      'black-gray' : '#24232C',
+      'green' : '#A4FFAF',
+      'red' : '#F64A4A',
+      'orange' : '#FB7C58',
+      'yellow' : '#FBCD65',
+      'white' : '#E6E5EA',
+      'gray' : '#817D92',
+    },
+    screens: {
+      'sm' : '375px',
+      'md' : '768px',
+      'lg' : '1440px'
+    },
     extend: {},
   },
   plugins: [],
